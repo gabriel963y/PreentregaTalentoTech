496ba57962a51ad9bb9f3f0336cecafaa8b033e1
gabriel963y <gabrielybarra963@gmail.com>
2025-07-16 15:24:51 -0300

    agregado

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import 'bootswatch/dist/Pulse/bootstrap.min.css';
+import 'bootswatch/dist/pulse/bootstrap.min.css';
 import { AuthProvider } from './context/AuthContext.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 
